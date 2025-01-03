7752ca24a021e9f2692a622743d7d7455010b2da
Halil İbrahim Direktör <hidirektor@gmail.com>
2025-01-03 21:55:43 +0300

    Jitpack Packaging

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,6 @@
 module me.t3sl4.util.version {
+    requires com.google.gson;
+    requires org.apache.httpcomponents.httpclient;
+
     exports me.t3sl4.util.version;
-    exports me.t3sl4.util.version.exception;
-    exports me.t3sl4.util.version.model;
 }
